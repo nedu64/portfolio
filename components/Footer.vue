5edafe351430e63916e611dc403cf53c0bcdbc40
nedu64 <chieduemmanuel68@gmail.com>
2025-07-14 10:05:51 +0100

    fix: footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -17,7 +17,7 @@
 
 <template>
     <footer class="text-white max-w-[1700px] mx-auto">
-        <nav class="fixed bottom-0 pl-8 pb-8 w-full z-1">
+        <nav class="fixed bottom-0 pl-0 pb-8 md:pl-8 w-full z-1">
             <ul class="flex gap-x-10 justify-center md:flex-col gap-y-8 bottom-0 left-0 min-w-10">
                 <li v-for="(item, index) in socialNavigation" :key="index">
                     <Icon :name="item.icon" class="text-primary-light" size="2em" />
